eee712b1d47b68b89796baca2a909e92a794633e
Afia <sanjidaakterafia0307@gmail,com>
2022-11-27 21:48:42 +0600

    finaltask completed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 var jwt = require('jsonwebtoken');
+const { query } = require('express');
+const stripe = require("stripe")(process.env.STRIPE_SK);
 const app = express()
 const port = process.env.PORT || 7000
 
@@ -42,30 +44,31 @@ async function run() {
     const categoriesCollection = client.db("bookRestore").collection("categories")
     const usersCollection = client.db("bookRestore").collection("users")
     const bookingsCollection = client.db("bookRestore").collection("bookings")
+    const paymentsCollection = client.db("bookRestore").collection("payments")
 
-      // admin verify 
+    // admin verify 
     const verifyAdmin = async (req, res, next) => {
       const decodedEmail = req.decoded.email;
       const query = { email: decodedEmail };
       const user = await usersCollection.findOne(query);
 
       if (user?.role !== 'Admin') {
-          return res.status(403).send({ message: 'forbidden access' })
+        return res.status(403).send({ message: 'forbidden access' })
       }
       next();
-  }
+    }
 
-  // Seller verify
+    // Seller verify
     const verifySeller = async (req, res, next) => {
       const decodedEmail = req.decoded.email;
       const query = { email: decodedEmail };
       const user = await usersCollection.findOne(query);
 
       if (user?.role !== 'Seller') {
-          return res.status(403).send({ message: 'forbidden access' })
+        return res.status(403).send({ message: 'forbidden access' })
       }
       next();
-  }
+    }
 
 
     app.get("/jwt", async (req, res) => {
@@ -96,8 +99,32 @@ async function run() {
     app.get("/categories/:name", async (req, res) => {
       const genre = req.params.name;
       console.log(genre)
-      const query = { genre: genre }
+      const query = { genre: genre,status:"Available" }
       const books = await bookCollection.find(query).toArray()
+
+      res.send(books)
+    })
+    app.get("/booksAd", async (req, res) => {
+      // const genre = req.params.name;
+      // console.log(genre)
+      const query = { advertise: true, status: "Available" }
+      const books = await bookCollection.find(query).toArray()
+
+      res.send(books)
+    })
+    app.get("/books/reported", async (req, res) => {
+     
+      const query={reported:true}
+     
+      const books = await bookCollection.find(query).toArray()
+
+      res.send(books)
+    })
+    app.delete("/books/reported/:id", async (req, res) => {
+     const id=req.params.id
+      const query={_id:ObjectId(id)}
+     
+      const books = await bookCollection.deleteOne(query)
 
       res.send(books)
     })
@@ -111,12 +138,12 @@ async function run() {
     })
 
     app.get('/advertiseBooks', async (req, res) => {
-      const query = { advertise: true }
+      const query = { advertise: true,status:"Available" }
       const books = await bookCollection.find(query).limit(10).toArray()
       res.send(books)
     })
 
-    app.post('/books',verifyJWT,verifySeller, async (req, res) => {
+    app.post('/books', verifyJWT, verifySeller, async (req, res) => {
       const user = req.body
       const post = Date()
       const result = await bookCollection.insertOne({ ...user, post: post })
@@ -151,7 +178,7 @@ async function run() {
     })
 
     // seller
-    app.get("/sellers",verifyJWT,verifyAdmin, async (req, res) => {
+    app.get("/sellers", verifyJWT, verifyAdmin, async (req, res) => {
 
       const query = { role: "Seller" }
       const sellers = await usersCollection.find(query).toArray()
@@ -160,7 +187,7 @@ async function run() {
       res.send(sellers)
     })
 
-    app.delete('/sellers/:id',verifyJWT,verifyAdmin, async (req, res) => {
+    app.delete('/sellers/:id', verifyJWT, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) }
       const result = await usersCollection.deleteOne(query)
@@ -169,8 +196,9 @@ async function run() {
     })
 
 
-    app.put('/sellers/:id',verifyJWT,verifyAdmin, async (req, res) => {
+    app.put('/sellers/:id', verifyJWT, verifyAdmin, async (req, res) => {
       const id = req.params.id;
+      console.log(id)
       const query = { _id: ObjectId(id), role: "Seller" }
 
       const seller = await usersCollection.findOne(query)
@@ -182,14 +210,33 @@ async function run() {
         },
       };
       const result = await bookCollection.updateOne(filter, updateDoc)
+      const resultUser = await usersCollection.updateOne(query, updateDoc)
       console.log(result)
-      res.send(result)
+      res.send(resultUser)
     })
+
+    // temporary update
+
+
+    // app.get('/allBooks', async (req, res) => {
+
+    //   const query = {}
+
+
+    //   const updateDoc = {
+    //     $set: {
+    //       reported:false
+    //     },
+    //   };
+    //   const result = await bookCollection.updateMany(query, updateDoc)
+    //   console.log(result)
+    //   res.send(result)
+    // })
 
 
     // buyers 
 
-    app.get("/buyers",verifyJWT,verifyAdmin, async (req, res) => {
+    app.get("/buyers", verifyJWT, verifyAdmin, async (req, res) => {
 
       const query = { role: "Buyer" }
       const sellers = await usersCollection.find(query).toArray()
@@ -198,7 +245,7 @@ async function run() {
       res.send(sellers)
     })
 
-    app.delete('/buyers/:id',verifyJWT,verifyAdmin, async (req, res) => {
+    app.delete('/buyers/:id', verifyJWT, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) }
       const result = await usersCollection.deleteOne(query)
@@ -206,6 +253,14 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/bookings/:email',verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      console.log(email)
+      const query = { buyerEmail: email }
+      const buyerBookings = await bookingsCollection.find(query).toArray()
+      console.log(buyerBookings)
+      res.send(buyerBookings)
+    })
 
 
     // seller-books
@@ -236,7 +291,7 @@ async function run() {
     })
 
 
-    app.put('/books/:id', async (req, res) => {
+    app.put('/books/:id',verifyJWT,verifySeller, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) }
 
@@ -255,12 +310,84 @@ async function run() {
 
     // booking
 
+    app.post('/create-payment-intent', async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: amount,
+        "payment_method_types": [
+          "card"
+        ]
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      console.log(payment)
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId
+      const filter = { _id: ObjectId(id) }
+      console.log(payment.sellerEmail)
+      const query={sellerEmail:payment.sellerEmail,name:payment.book}
+      const updatedDoc = {
+        $set: {
+          status:"Sold",
+          transactionId: payment.transactionId
+        }
+      }
+      const updatedBook = {
+        $set: {
+          status:"Sold",
+         
+        }
+      }
+      const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+      const updatedbook = await bookCollection.updateOne(query, updatedBook)
+
+      console.log(updatedbook,updatedResult)
+      res.send(result);
+    })
+
+
+    app.get('/payment/:id', async (req, res) => {
+
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await bookingsCollection.findOne(query)
+      console.log(result)
+      res.send(result)
+
+    })
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body
       const result = await bookingsCollection.insertOne(booking)
       console.log(result)
       res.send(result)
     })
+
+    app.put("/books/report/:id",async(req,res)=>{
+      const id=req.params.id;
+
+      const query={_id:ObjectId(id)}
+
+      const upDoc={
+        $set:{
+          reported:true
+        }
+      }
+
+      const reportItem=await bookCollection.updateOne(query,upDoc)
+      res.send(reportItem)
+    })
+
+
 
   }
 
